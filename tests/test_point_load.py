7b2caf3f7a03fbf9102d41de1d23fbf55e342f1c
Brett Mattas <bmattas2@gmail.com>
2026-06-19 07:22:38 -0500

    Finished stress tests for point load

--- tests/test_point_load.py
+++ tests/test_point_load.py
@@ -1,4 +1,4 @@
-from math import atan2, pi
+from math import atan2, pi, sqrt, cos, sin
 
 from buried_structures.point import Origin, Point_3d
 from buried_structures.point_load import Point_Load
@@ -42,7 +42,92 @@ def test_sigma_z():
     result = load.stress_z(p1)
     
     assert result == pytest.approx(expected)
+    
+def test_sigma_r():
+    # INclude a distance
+    p3 = Point_3d(2, 3, 1)
+    distance = sqrt(2**2 + 3**2 + 1**2)
+    r_distance = sqrt(2**2 + 3**2)
+    z3 = 1
 
+    expected = -magnitude/(2*pi*distance**2) * (
+            -3*r_distance**2*z3/distance**3 + (1-2*poisson)*distance/(
+                distance+z3
+                ))
+    result = load.stress_r(p3)
+    assert result == pytest.approx(expected)
+    
+def test_sigma_theta():
+    # INclude a distance
+    p3 = Point_3d(2, 3, 1)
+    distance = sqrt(2**2 + 3**2 + 1**2)
+    # r_distance = sqrt(2**2 + 3**2)
+    z3 = 1
+
+    expected = -(1-2*poisson)*magnitude/(2*pi*distance**2) * (
+            z3/distance - distance/(distance + z3))
+    result = load.stress_theta(p3)
+    assert result == pytest.approx(expected)
+
+def test_sigma_x():
+    # INclude a distance
+    p1 = Point_3d(2, 0, 1)
+    # distance = sqrt(2**2 + 0**2 + 1**2)
+    # r_distance = sqrt(2**2 + 0**2)
+    # z3 = 1
+
+    expected = load.stress_r(p1)
+    result = load.stress_x(p1)
+    assert result == pytest.approx(expected)
+    
+    p2 = Point_3d(3, 2, 10)
+    # distance = sqrt(3**2 + 2**2 + 10**2)
+    # r_distance = sqrt(3**2 + 2**2)
+    # z2 = 10
+    
+    result = load.stress_x(p2)
+    print("load.point:")
+    print(load.center)
+    print(f"o.theta = {o.theta(p2)}")
+
+    pr = load.stress_r(p2)
+    pt = load.stress_theta(p2)
+
+    theta = atan2(2, 3)
+    print(f"theta={theta}")
+    expected = pr*abs(cos(theta)) + pt*abs(sin(theta))
+    
+    assert result == pytest.approx(expected)
+
+def test_sigma_y():
+    # INclude a distance
+    p1 = Point_3d(0, 2, 1)
+    # distance = sqrt(2**2 + 0**2 + 1**2)
+    # r_distance = sqrt(2**2 + 0**2)
+    # z3 = 1
+
+    expected = load.stress_r(p1)
+    result = load.stress_y(p1)
+    assert result == pytest.approx(expected)
+    
+    p2 = Point_3d(3, 2, 10)
+    # distance = sqrt(3**2 + 2**2 + 10**2)
+    # r_distance = sqrt(3**2 + 2**2)
+    # z2 = 10
+    
+    result = load.stress_y(p2)
+    print("load.point:")
+    print(load.center)
+    print(f"o.theta = {o.theta(p2)}")
+
+    pr = load.stress_r(p2)
+    pt = load.stress_theta(p2)
+
+    theta = atan2(2, 3)
+    print(f"theta={theta}")
+    expected = pr*abs(sin(theta)) + pt*abs(cos(theta))
+    
+    assert result == pytest.approx(expected)
 def test_bad_poisson():
     # Poisson limits
 
@@ -51,3 +136,7 @@ def test_bad_poisson():
         
     with pytest.raises(ValueError):
         bad_load = Point_Load(o, 1.0, 0.50001)
+        
+def test_r():
+
+    pass
