a6a3a92a4d77edcf3b0383d980898278246d4dff
Brett Mattas <bmattas2@gmail.com>
2026-07-06 06:28:49 -0500

    Added add_load tests

--- tests/test_add_load.py
+++ tests/test_add_load.py
@@ -3,11 +3,13 @@ from math import atan2, pi, sqrt, cos, sin
 from buried_structures.point import Origin, Point_3d
 from buried_structures.add_load import Add_Load
 from buried_structures.point_load import Point_Load
+from buried_structures.point_load import Rectangular_Load
 import pytest
 
 magnitude = 14.0
 poisson = 0.30
 o = Origin()
+p = Point_3d(12, 0, 0)
 load = Point_Load(o, magnitude, poisson)
 empty_add = Add_Load([])
 
@@ -16,6 +18,16 @@ single_add = Add_Load([load])
 point = Point_3d(0, 0, 12)
 
 double_add = Add_Load([load, load])
+
+length, width = 16, 32
+distributed_magnitude = magnitude / (length * width)
+rectangular_load = Rectangular_Load(o, 
+                                   distributed_magnitude,
+                                   length, width)
+rectangular_load_2 = Rectangular_Load(p, 
+                                   distributed_magnitude,
+                                   length, width)
+multi_add = Add_Load([load, rectangular_load, rectangular_load_2])
 
 def test_type():
     assert empty_add.load_type == "Add Load"
@@ -52,6 +64,19 @@ def test_double():
     expected = 2*load.stress_z(point)
     assert result == expected
     
+def test_rect():
+
+    result = multi_add.stress_x(point)
+    expected = multi_add.stress_x(point)
+    assert result == expected
+    
+    result = multi_add.stress_y(point)
+    expected = multi_add.stress_y(point)
+    assert result == expected
+    
+    result = multi_add.stress_z(point)
+    expected = multi_add.stress_z(point)
+    assert result == expected
 def test_fails():
 
     with pytest.raises(ValueError):
